0340663718b4c6e0723cca3d3d311e7f3c81475f
Janet Ishola <janetishola2010@gmail.com>
2022-09-07 15:21:19 +0100

    staking contract

---
+++ src/main/java/com/arnAAVE/java_sdk/contractModels/IStakedToken.java
@@ -0,0 +1,146 @@
+package com.arnAAVE.java_sdk.contractModels;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.RemoteFunctionCall;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 4.8.7.
+ */
+@SuppressWarnings("rawtypes")
+public class IStakedToken extends Contract {
+    public static final String BINARY = "";
+
+    public static final String FUNC_REWARD_TOKEN = "REWARD_TOKEN";
+
+    public static final String FUNC_STAKED_TOKEN = "STAKED_TOKEN";
+
+    public static final String FUNC_CLAIMREWARDS = "claimRewards";
+
+    public static final String FUNC_COOLDOWN = "cooldown";
+
+    public static final String FUNC_REDEEM = "redeem";
+
+    public static final String FUNC_STAKE = "stake";
+
+    @Deprecated
+    protected IStakedToken(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    public IStakedToken(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected IStakedToken(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected IStakedToken(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteFunctionCall<String> REWARD_TOKEN() {
+        final Function function = new Function(FUNC_REWARD_TOKEN, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteFunctionCall<String> STAKED_TOKEN() {
+        final Function function = new Function(FUNC_STAKED_TOKEN, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> claimRewards(String to, BigInteger amount) {
+        final Function function = new Function(
+                FUNC_CLAIMREWARDS, 
+                Arrays.<Type>asList(new Address(160, to),
+                new org.web3j.abi.datatypes.generated.Uint256(amount)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> cooldown() {
+        final Function function = new Function(
+                FUNC_COOLDOWN, 
+                Arrays.<Type>asList(), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> redeem(String to, BigInteger amount) {
+        final Function function = new Function(
+                FUNC_REDEEM, 
+                Arrays.<Type>asList(new Address(160, to),
+                new org.web3j.abi.datatypes.generated.Uint256(amount)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteFunctionCall<TransactionReceipt> stake(String onBehalfOf, BigInteger amount) {
+        final Function function = new Function(
+                FUNC_STAKE, 
+                Arrays.<Type>asList(new Address(160, onBehalfOf),
+                new org.web3j.abi.datatypes.generated.Uint256(amount)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    @Deprecated
+    public static IStakedToken load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new IStakedToken(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static IStakedToken load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new IStakedToken(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static IStakedToken load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return new IStakedToken(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static IStakedToken load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new IStakedToken(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<IStakedToken> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(IStakedToken.class, web3j, credentials, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<IStakedToken> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(IStakedToken.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    }
+
+    public static RemoteCall<IStakedToken> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return deployRemoteCall(IStakedToken.class, web3j, transactionManager, contractGasProvider, BINARY, "");
+    }
+
+    @Deprecated
+    public static RemoteCall<IStakedToken> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(IStakedToken.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    }
+}
