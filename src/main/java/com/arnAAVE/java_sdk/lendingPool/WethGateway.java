d247994cb5edd350f071d835af6fd71325c1068b
EhigieO <eshioke@gmail.com>
2022-09-27 09:40:43 +0100

    bout to merge from master

--- src/main/java/com/arnAAVE/java_sdk/lendingPool/WethGateway.java
+++ src/main/java/com/arnAAVE/java_sdk/lendingPool/WethGateway.java
@@ -19,15 +19,37 @@ public class WethGateway {
         weth = new IWETHGateway(assetAddress,connection.getWeb3j(),connection.getCredentials(),provider);
     }
 
-    public TransactionReceipt depositEth(String onBehalfOf, String poolAddress) throws Exception {
+//    public TransactionReceipt depositEth(String onBehalfOf, String poolAddress) throws Exception {
+//        public RemoteFunctionCall<TransactionReceipt> newGreeting(String _greeting, BigInteger weiValue) {
+//            final Function function = new Function(
+//                    FUNC_NEWGREETING,
+//                    Arrays.<Type>asList(new org.web3j.abi.datatypes.Utf8String(_greeting)),
+//                    Collections.<TypeReference<?>>emptyList());
+//            return executeRemoteCallTransaction(function, weiValue);
+//        }
+//        Function function = new Function<>(
+//                "functionName",  // function we're calling
+//                Arrays.asList(new Type(value), ...),  // Parameters to pass as Solidity Types
+//        Arrays.asList(new TypeReference<Type>() {}, ...));
+//
+//        String encodedFunction = FunctionEncoder.encode(function)
+//        Transaction transaction = Transaction.createFunctionCallTransaction(
+//                <from>, <gasPrice>, <gasLimit>, contractAddress, <funds>, encodedFunction);
+//
+//        org.web3j.protocol.core.methods.response.EthSendTransaction transactionResponse =
+//                web3j.ethSendTransaction(transaction).sendAsync().get();
 
-        return weth.depositETH(poolAddress,onBehalfOf, BigInteger.ZERO).send();
-    }
+//        String transactionHash = transactionResponse.getTransactionHash();
+//
+//// wait for response using EthGetTransactionReceipt...
+//        return weth.depositETH(poolAddress,onBehalfOf, BigInteger.ZERO).send();
+//    }
 
 
-    public TransactionReceipt withdrawEth(String onBehalfOf, String amount,String poolAddress) {
+
+    public TransactionReceipt withdrawEth(String onBehalfOf, String amount,String poolAddress) throws Exception {
         BigInteger value = new BigInteger(amount);
-        weth.withdrawETH(poolAddress,value, onBehalfOf);
-        return null;
+
+    return weth.withdrawETH(poolAddress,value, onBehalfOf).send();
     }
 }
